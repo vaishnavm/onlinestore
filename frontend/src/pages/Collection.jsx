10bb9737e2a4c43eda4650433e75e91e419ea966
vaishnavm <vaishnav.nadal@gmail.com>
2026-05-18 14:33:07 +0400

    filter option added in collection page

--- frontend/src/pages/Collection.jsx
+++ frontend/src/pages/Collection.jsx
@@ -22,15 +22,37 @@ const Collection = () => {
 
   }
 
-  useEffect(()=>{
-    setFilterProducts(products);
-    //console.log(products);
-  },[])
+  const toggleSubCategory=(e)=>{
+    if(subCategory.includes(e.target.value)){
+      setSubCategory(prev=>prev.filter(item=>item!==e.target.value))
+
+    }else{
+      setSubCategory(prev=>[...prev,e.target.value])
+    }
+
+  }
+
+  const applyFilter = ()=>{
+    let productsCopy=products.slice()
+    if(category.length>0){
+      productsCopy=productsCopy.filter(item=>category.includes(item.category))
+    }
+    if(subCategory.length>0){
+      productsCopy=productsCopy.filter(item=>subCategory.includes(item.subCategory))
+    }
+
+    setFilterProducts(productsCopy)
+  }
+
+  // useEffect(()=>{
+  //   setFilterProducts(products);
+  //   //console.log(products);
+  // },[])
 
   useEffect(()=>{
-    console.log(category)
+    applyFilter()
 
-  },[category])
+  },[category,subCategory])
 
   return (
     <div className='flex flex-col sm:flex-row gap-1 sm:gap-10 pt-10 border-t'>
@@ -62,13 +84,13 @@ const Collection = () => {
           <p className='mb-3 text-sm font-medium'>TYPE</p>
           <div className='text-gray-700 flex flex-col gap-2 text-sm font-light'>
             <p className='flex gap-2'>
-            <input className='w-3' type="checkbox" value={'Topwear'}/>Topwear  
+            <input className='w-3' type="checkbox" value={'Topwear'} onChange={toggleSubCategory}/>Topwear  
             </p> 
             <p className='flex gap-2'>
-            <input className='w-3' type="checkbox" value={'Bottomwear'}/>Bottomwear  
+            <input className='w-3' type="checkbox" value={'Bottomwear'} onChange={toggleSubCategory}/>Bottomwear  
             </p> 
             <p className='flex gap-2'>
-            <input className='w-3' type="checkbox" value={'Winterwear'}/>Winterwear  
+            <input className='w-3' type="checkbox" value={'Winterwear'} onChange={toggleSubCategory}/>Winterwear  
             </p> 
 
           </div>
