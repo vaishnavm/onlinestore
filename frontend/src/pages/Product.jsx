bb0b03409c370f46c146bdc03f9db5f7ba6a1f77
vaishnavm <vaishnav.nadal@gmail.com>
2026-05-19 23:14:55 +0400

    feat: add product details page content

--- frontend/src/pages/Product.jsx
+++ frontend/src/pages/Product.jsx
@@ -1,13 +1,16 @@
 import React, { useContext, useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
 import { ShopContext } from '../context/ShopContext'
+import { assets } from '../assets/assets'
 
 const Product = () => {
 
   const {productId}=useParams()
-  const {products}=useContext(ShopContext)
+  const {products,currency}=useContext(ShopContext)
   const [productData,setProductData]=useState(false)
   const [image,setImage]=useState('')
+  const [size,setSize]=useState('')
+  const [quantity,setQuantity]=useState(1)
 
   const fetchProductData=async ()=>{
     products.map((item)=>{
@@ -29,16 +32,71 @@ const Product = () => {
       <div className='flex gap-12 sm:gap-12 flex-col sm:flex-row'>
         {/* Product Image */}
         <div className='flex-1 flex flex-col-reverse gap-3 sm:flex-row ' >
-          <div className='flex sm:flex-col overflow-x-auto sm:overflow-y-scroll justify-between'>
+          <div className='flex sm:flex-col overflow-x-auto sm:overflow-y-scroll justify-between sm:justify-normal sm:w-[18.7%] w-full'>
             {
               productData.image.map((item,index)=> (
-                <img src={item} key={index} className='w-[24%] sm:w-full sm mb-3 flex-shrink-0 cursor-pointer' alt=''/>
+                <img onClick={()=>setImage(item)} src={item} key={index} className='w-[24%] sm:w-full sm mb-3 flex-shrink-0 cursor-pointer' alt=''/>
               )
               )
             }
           </div>
+
+          {/* Product Image Selected */}
+          <div className='w-full sm:w-[80%]'>
+            <img className='w-full h-auto' src={image} alt="" />
+          </div>
+        </div>
+        
+        {/* Product Info */}
+        <div className='flex-1'>
+          <h1 className='font-medium text-2xl mt-2'>{productData.name}</h1>
+          <div className='flex item-center gap-1 mt-2'>
+            <img src={assets.star_icon} alt="" className="w-3 5" />
+            <img src={assets.star_icon} alt="" className="w-3 5" />
+            <img src={assets.star_icon} alt="" className="w-3 5" />
+            <img src={assets.star_icon} alt="" className="w-3 5" />
+            <img src={assets.star_dull_icon} alt="" className="w-3 5" />
+            <p className='pl-2'>(122)</p>
+          </div>
+          <p className='mt-5 text-3xl font-medium'>{currency}{productData.price}</p>
+          <p className='mt-5 text-gray-500 md:w-4/5'>{productData.description}</p>
+          <div className='flex flex-col my-8 gap-4'>
+              <p>Select Size</p>
+              <div className='flex gap-2'>
+                {
+                  productData.sizes.map((item,index)=>(
+                    <button onClick={()=>setSize(item)} className={`border py-2 px-4 bg-gray-100 ${item===size? 'border-orange-500':''}`} key={index}>{item}</button>
+                  ))
+                }
+              </div>
+          </div>
+          <button className='bg-black text-white px-8 py-3 text-sm active:bg-gray-700'>ADD TO CART</button>
+          <hr className='mt-8 sm:w-4/5'/>
+          <div className='text-sm text-gray-500 mt-5 flex flex-col gap-1'>
+            <p>100% Original Product.</p>
+            <p>Cash on delivery is available on this product.</p>
+            <p>Easy returm and exchange policy within 7 days.</p>
+          </div>
+        </div>
+
+      </div>
+
+      {/*Product Description and Review Section */}
+      <div className='mt-20'>
+        <div className='flex'>
+          <b className='border px-5 py-3 text-sm'>Description</b>
+          <p className='border px-5 py-3 text-sm'>Reviews (122)</p>
+        </div>
+        <div className='flex flex-col gap-4 border px-6 py-6 text-sm text-gray-500'>
+          <p>Modern eCommerce platform built with the MERN stack, offering a fast and responsive shopping experience.
+Users can browse products, view detailed product pages, add items to cart, and place secure orders online.</p>
+          <p>Includes user authentication, product management, search functionality, and responsive UI for all devices.
+Designed with scalable architecture and clean user interface to deliver smooth performance and easy navigation.</p>
         </div>
       </div>
+
+      {/*Related Products Section */}
+      
 
     </div>
   ) : <div className='opacity-0'></div>
