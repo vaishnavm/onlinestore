8af7fa1ce0cd7e386abc1cd817bb8934a1141fdc
unknown <vaishnav.nadal@gmail.com>
2026-05-21 12:47:00 +0400

    feat: add orders page

--- frontend/src/context/ShopContext.jsx
+++ frontend/src/context/ShopContext.jsx
@@ -1,6 +1,7 @@
 import React, { createContext, use, useEffect, useState } from "react";
 import { products } from "../assets/assets";
 import { toast } from "react-toastify";
+import {useNavigate} from 'react-router-dom'
 
  export const ShopContext=createContext();
 
@@ -11,6 +12,7 @@ const ShopContextProvider = (props) => {
     const [search,setSearch]=useState('')
     const [showSearch,setShowSearch]=useState(false)
     const [cartItems,setCartItems]=useState({})
+    const navigate= useNavigate()
 
     const addToCart=async (itemId,size)=>{
 
@@ -93,7 +95,8 @@ const ShopContextProvider = (props) => {
        products,currency,delivery_fee,
        search,setSearch,showSearch,setShowSearch,
        cartItems,addToCart,
-       getCartCount,updateQuantity,getCartAmount
+       getCartCount,updateQuantity,getCartAmount,
+       navigate
     }
 
   return (
