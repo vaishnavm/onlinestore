fdb99fb8a85e04db23cae1ffa2b944904c017430
vaishnavm <vaishnav.nadal@gmail.com>
2026-05-22 22:46:23 +0400

    feat: connect Cloudinary

--- backend/server.js
+++ backend/server.js
@@ -2,11 +2,13 @@ import express from "express"
 import cors from "cors"
 import 'dotenv/config'
 import connectDB from "./config/mongodb.js"
+import connectCloudinary from "./config/cloudinary.js"
 
 //App config
 const app = express()
 const port = process.env.PORT || 4000
 connectDB()
+connectCloudinary()
 
 //Middlewares
 app.use(express.json())
