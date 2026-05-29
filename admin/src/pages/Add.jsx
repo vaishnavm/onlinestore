7e799d9785a83dcb1e33786d7d2e195343c27419
unknown <vaishnav.nadal@gmail.com>
2026-05-29 17:49:12 +0400

    feat: initiate product upload in admin

--- admin/src/pages/Add.jsx
+++ admin/src/pages/Add.jsx
@@ -1,10 +1,35 @@
 import React from 'react'
+import { assets } from '../assets/assets'
 
 const Add = () => {
   return (
-    <div>
-      add
-    </div>
+    <form>
+      <div>
+        <p>Upload Image</p>
+
+        <div className='flex gap-2'>
+          <label htmlFor="image1">
+            <img className='w-20' src={assets.upload_area} alt="" />
+            <input type='file' id='image1' hidden></input>
+          </label>
+
+          <label htmlFor="image2">
+            <img className='w-20' src={assets.upload_area} alt="" />
+            <input type='file' id='image2' hidden></input>
+          </label>
+
+          <label htmlFor="image3">
+            <img className='w-20' src={assets.upload_area} alt="" />
+            <input type='file' id='image3' hidden></input>
+          </label>
+
+          <label htmlFor="image4">
+            <img className='w-20' src={assets.upload_area} alt="" />
+            <input type='file' id='image4' hidden></input>
+          </label>
+        </div>
+      </div>
+    </form>
   )
 }
 
