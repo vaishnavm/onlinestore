0375a3ea0ccb39a15187464d9aa96a518debc5fe
unknown <vaishnav.nadal@gmail.com>
2026-06-01 16:32:39 +0400

    feat: add list products in admin panel

--- admin/src/pages/List.jsx
+++ admin/src/pages/List.jsx
@@ -1,10 +1,78 @@
 import React from 'react'
+import {useState, useEffect} from 'react'
+import axios from 'axios'
+import { backendUrl, currency } from '../App'
+import { toast } from 'react-toastify'
 
-const List = () => {
-  return (
-    <div>
+const List = ({token}) => {
+  const [list,setList]=useState([])
+
+   const fetchList=async ()=>{
+
+    try {
+      const response=await axios.get(backendUrl+'/api/product/list')
+      //console.log(response.data)
+
+      if(response.data.success){
+          setList(response.data.products)
+      }else{
+        toast.error(response.data.error)
+      }
+    } catch (error) {
+      console.log(error);
+      toast.error(error.message)
       
-    </div>
+    }
+
+   }
+
+   const removeProduct=async (id)=>{
+      try {
+        const response= await axios.post(backendUrl+'/api/product/remove',{id},{headers:{token}})
+        if(response.data.success){
+          toast.success(response.data.message)
+          await fetchList()
+        }else{
+          toast.error(response.data.message)
+        }
+      } catch (error) {
+        console.log(error);
+      toast.error(error.message)
+      }
+   }
+
+   useEffect(()=>{
+    fetchList()
+   },[])
+
+  return (
+    <>
+      <p className='mb-2'>All Products List</p>
+      <div className='flex flex-col gap-2'>
+        {/* -- List Table Title --*/}
+        <div className='hidden md:grid grid-cols-[1fr_3fr_1fr_1fr_1fr] items-center py-1 px-2 border bg-gray-100 text-sm'>
+          <b>Image</b>
+          <b>Name</b>
+          <b>Category</b>
+          <b>Price</b>
+          <b className='text-center'>Action</b>
+        </div>
+
+        {/* Product List */}
+
+        {
+          list.map((item,index)=>(
+              <div className='grid grid-cols-[1fr_3fr_1fr] md:grid-cols-[1fr_3fr_1fr_1fr_1fr] items-center gap-2 py-1 px-2 border text-sm border-gray-100' key={index}>
+                <img src={item.image[0]} className='w-12' alt="" />
+                <p>{item.name}</p>
+                <p>{item.category}</p>
+                <p>{currency}{item.price}</p>
+                <p onClick={()=>removeProduct(item._id)} className='text-lg text-right md:text-center cursor-pointer'>X</p>
+              </div>
+          ))
+        }
+      </div>
+    </>
   )
 }
 
