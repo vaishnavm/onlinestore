3759c4afe7ddbd6783e9a1fef450f0c38b69e958
unknown <vaishnav.nadal@gmail.com>
2026-06-05 16:14:50 +0400

    feat: integrate user order list with backend API

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -54,6 +54,14 @@ const allOrders = async (req,res)=>{
 //User order data for frontend
 
 const userOrders = async (req,res)=>{
+    try {
+        const {userId}=req.body
+        const orders=await orderModel.find({userId})
+        res.json({success:true, orders})
+    } catch (error) {
+         console.log(error);
+        res.json({success:false , message:error.message})
+    }
     
 }
 
